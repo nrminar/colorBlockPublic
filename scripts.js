c418dc3f2a537329ea7ca5f7b863988e38e0ac14
Nolan Minar <nolanminar@Nolans-MacBook-Air.local>
2019-08-29 10:24:55 -0500

    styling

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ $( document ).ready(redNow);
 $( document ).ready(yelNow);
 $( document ).ready(bluNow);
 $( document ).ready(greNow);
-
 let redCount = 0;
 let yelCount = 0;
 let bluCount = 0;
@@ -20,40 +19,120 @@ function bluNow(){
 function greNow(){
     $( '#greenButton' ).on( 'click', addGre)
 }
-
-function addRed(){
-    redCount++;
+function removeBlock(rBlock){
+    $(rBlock).remove();
     let rCountIn = $( '#redCount' );
-    rCountIn.empty();
-    rCountIn.append('Red Count:',redCount);
+    let yCountIn = $( '#yellowCount' );
+    let bCountIn = $( '#blueCount' );
+    let gCountIn = $( '#greenCount' );
+    console.log('removed Block');
+    console.log(rBlock);
+    if(rBlock.id == 'redBox'){
+        redCount -= 1;
+        rCountIn.empty();
+        rCountIn.append('Red Count:',redCount);
+    }else if(rBlock.id == 'yelBox'){
+        yelCount -= 1;
+        yCountIn.empty();
+        yCountIn.append('Yellow Count:',yelCount);
+    }else if(rBlock.id == 'bluBox'){
+        bluCount -= 1;
+        bCountIn.empty();
+        bCountIn.append('Blue Count:',bluCount);
+
+    }else if(rBlock.id == 'greBox'){
+        greCount -= 1;
+        gCountIn.empty();
+        gCountIn.append('Green Count:',greCount);
+    }
+}
+function addRed(){
+    let blockMultiplier = $( '#multiBlock' ).val();
+    let rCountIn = $( '#redCount' );
     let colorCon = $( '#colorContainer' );
-    colorCon.append('<p id="redBox"></p>');
-    console.log('added red');
+    if(blockMultiplier != ''){
+        for(i=0; i<blockMultiplier; i++){
+            redCount++;
+            rCountIn.empty();
+            rCountIn.append('Red Count:',redCount);
+            colorCon.append('<button id="redBox" onclick="removeBlock(this)"></button>');
+            console.log('added red');
+        }
+    }else{
+        redCount++;
+        rCountIn.empty();
+        rCountIn.append('Red Count:',redCount);
+        colorCon.append('<button id="redBox" onclick="removeBlock(this)"></button>');
+        console.log('added red');
+    }
+    $( '#multiBlock' ).val( '' );
 }
 function addYel(){
-    yelCount++;
+    let blockMultiplier = $( '#multiBlock' ).val();
     let yCountIn = $( '#yellowCount' );
-    yCountIn.empty();
-    yCountIn.append('Yellow Count:',yelCount);
     let colorCon = $( '#colorContainer' );
-    colorCon.append('<p id="yelBox"></p>');
-    console.log('added yellow');
+    if(blockMultiplier != ''){
+        for(i=0; i<blockMultiplier; i++){
+            yelCount++;
+            yCountIn.empty();
+            yCountIn.append('Yellow Count:',yelCount);
+            colorCon.append('<button id="yelBox"  onclick="removeBlock(this)"></button>');
+            console.log('added yellow');
+        }
+    }else{
+        yelCount++;
+        yCountIn.empty();
+        yCountIn.append('Yellow Count:',yelCount);
+        colorCon.append('<button id="yelBox"  onclick="removeBlock(this)"></button>');
+        console.log('added yellow');
+    }
+    $( '#multiBlock' ).val( '' );
 }
 function addBlu(){
-    bluCount++;
+    let blockMultiplier = $( '#multiBlock' ).val();
     let bCountIn = $( '#blueCount' );
-    bCountIn.empty();
-    bCountIn.append('Blue Count:',bluCount);
     let colorCon = $( '#colorContainer' );
-    colorCon.append('<p id="bluBox"></p>');
-    console.log('added blue');
+
+    if(blockMultiplier != ''){
+        for(i=0; i<blockMultiplier; i++){
+            bluCount++;
+            bCountIn.empty();
+            bCountIn.append('Blue Count:',bluCount);
+            colorCon.append('<button id="bluBox"  onclick="removeBlock(this)"></button>');
+            console.log('added blue');
+        }
+    }else{
+        bluCount++;
+        bCountIn.empty();
+        bCountIn.append('Blue Count:',bluCount);
+        colorCon.append('<button id="bluBox"  onclick="removeBlock(this)"></button>');
+        console.log('added blue');
+    }
+    $( '#multiBlock' ).val( '' );
 }
 function addGre(){
-    greCount++;
+    let blockMultiplier = $( '#multiBlock' ).val();
     let gCountIn = $( '#greenCount' );
+    let colorCon = $( '#colorContainer' );
+    if(blockMultiplier != ''){
+        for(i=0; i<blockMultiplier; i++){
+            greCount++;
+            gCountIn.empty();
+            gCountIn.append('Green Count:',greCount);
+            colorCon.append('<button id="greBox"  onclick="removeBlock(this)"></button>');
+            console.log('added green');
+        }
+    }else{
+        greCount++;
+        gCountIn.empty();
+        gCountIn.append('Green Count:',greCount);
+        colorCon.append('<button id="greBox"  onclick="removeBlock(this)"></button>');
+        console.log('added green');
+    }
+    $( '#multiBlock' ).val( '' );
+
     gCountIn.empty();
     gCountIn.append('Green Count:',greCount);
-    let colorCon = $( '#colorContainer' );
-    colorCon.append('<p id="greBox"></p>');
+    colorCon.append('<button id="greBox"  onclick="removeBlock(this)"></button>');
     console.log('added green');
 }
